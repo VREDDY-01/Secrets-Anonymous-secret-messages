534eb718240471d5e7609ddd0bc8b3517f942b2d
K Vishnu Teja <vreddy2300@gmail.com>
2023-02-24 01:52:42 +0530

    Add Level 4 Salting and bcrypt
    Level 4 - Added b-crypt module and salting for passwords.

--- app.js
+++ app.js
@@ -3,70 +3,75 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 
 const app = express();
 
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.set("strictQuery",false);
+mongoose.set("strictQuery", false);
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
 const userSchema = new mongoose.Schema({
-    email:String,
-    password:String
+  email: String,
+  password: String,
 });
 
-const User = new mongoose.model("users",userSchema);
+const User = new mongoose.model("users", userSchema);
 
-app.get("/",(req,res)=>{
-    res.render("home");
+app.get("/", (req, res) => {
+  res.render("home");
 });
 
-app.get("/register",(req,res)=>{
-    res.render("register");
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
-app.get("/login",(req,res)=>{
-    res.render("login");
+app.get("/login", (req, res) => {
+  res.render("login");
 });
 
-app.post("/register",(req,res)=>{
+app.post("/register", (req, res) => {
+  bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
     const newUser = new User({
-        email:req.body.username,
-        password:md5(req.body.password)
+      email: req.body.username,
+      password: hash,
     });
 
-    newUser.save((err)=>{
-        if(!err){
-            res.render("secrets");
-        }else{
-            console.log(err);
-        }
-    })
-});
-
-app.post("/login", function(req, res){
-    const username = req.body.username;
-    const password = md5(req.body.password);
-  
-    User.findOne({email: username}, function(err, foundUser){
-      if (err) {
-        res.render("nouser");
+    newUser.save((err) => {
+      if (!err) {
+        res.render("secrets");
       } else {
-        if (foundUser) {
-          if (foundUser.password === password) {
-            res.render("secrets");
-          }
-        }else{
-            res.render("nouser");
-        }
+        console.log(err);
       }
     });
   });
+});
 
-app.listen(3000,()=>{
-    console.log("Server has started at port 3000");
+app.post("/login", function (req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({ email: username }, function (err, foundUser) {
+    if (err) {
+      res.render("nouser");
+    } else {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function (err, result) {
+          if (result === true) {
+            res.render("secrets");
+          }
+        });
+      } else {
+        res.render("nouser");
+      }
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server has started at port 3000");
 });
