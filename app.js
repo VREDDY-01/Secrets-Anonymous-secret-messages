61b846d876fb2e96fba876ab767bb5d46df92adb
K Vishnu Teja <vreddy2300@gmail.com>
2023-02-24 01:11:14 +0530

    Add Level 3 Hashing Passwords
    Level-3 Added Hashing for the passwords.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 
 const app = express();
 
@@ -18,8 +18,6 @@ const userSchema = new mongoose.Schema({
     email:String,
     password:String
 });
-
-userSchema.plugin(encrypt,{secret: process.env.SECRET, encryptedFields:["password"]});
 
 const User = new mongoose.model("users",userSchema);
 
@@ -38,7 +36,7 @@ app.get("/login",(req,res)=>{
 app.post("/register",(req,res)=>{
     const newUser = new User({
         email:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
 
     newUser.save((err)=>{
@@ -52,7 +50,7 @@ app.post("/register",(req,res)=>{
 
 app.post("/login", function(req, res){
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
   
     User.findOne({email: username}, function(err, foundUser){
       if (err) {
